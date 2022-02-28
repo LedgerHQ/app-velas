f53b8bd1709fc9b6fd82d374fa81593f1e96f29b
n4l5u0r <59141606+n4l5u0r@users.noreply.github.com>
2022-02-28 18:03:34 +0000

    fix: clang format libs

--- libsol/include/sol/message.h
+++ libsol/include/sol/message.h
@@ -2,7 +2,5 @@
 
 #include "parser.h"
 
-int process_message_body(
-    const uint8_t *message_body,
-    int message_body_length,
-    const MessageHeader *header);
+int process_message_body(const uint8_t *message_body, int message_body_length,
+                         const MessageHeader *header);
